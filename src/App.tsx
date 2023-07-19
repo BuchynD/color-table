6fca4420dbba817f316e9e13730f21c0d3735a67
BuchynD <buchindaniil6@gmail.com>
2023-07-19 18:46:40 +0300

    256^2 cells

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [blue, setBlue] = useState(0);
+
+  const redAndGreenValues = [];
+
+  for (let i = 0; i < 256; i++) {
+    for (let j = 0; j < 256; j++) {
+      redAndGreenValues.push([i, j]);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div>
+        <div className="table grid">
+          {redAndGreenValues.map(([red, green]) => <div
+            className="cell"
+            style={{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }}
+            key={red * 256 + green}></div>)}
+        </div>
+      </div>
+      <div className="slider">
+        <span>Blue:</span>
+        <input type="range" min={0} max={255} value={blue} onChange={event => setBlue(parseInt(event.target.value))} />
+        <input type="number" min={0} max={255} step={1} value={blue} onChange={event => setBlue(parseInt(event.target.value))} />
+      </div>
     </div>
   );
 }
